280c7f9817b7248bc1c35c0d8b008923fd68ed4c
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-08-13 14:53:27 +0300

    [+] AppModule : created StorageService and LanguageService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { StorageService } from './storage.service';
+import { LanguageService } from './language.service';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [
-      BrowserModule
-  ],
-  declarations: [
-      AppComponent
-  ],
-  providers: [
-
-  ],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    providers: [
+        StorageService,
+        LanguageService
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
