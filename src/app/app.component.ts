ac3393eebb84e2d4b58e16dee80542c5d9a8a6eb
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-08-13 14:56:39 +0300

    [*] AppComponent : added translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { LanguageService, Language } from './language.service';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'Vladimir Sinyavsky\'s Pages';
+    langListSource: Language[];
+
+    constructor(private _lang: LanguageService) {
+        this.langListSource = Language.all;
+    }
+
+    public get currentLang(): Language {
+        return this._lang.current;
+    }
+
+    public changeLang = (code: string) => {
+        if (!code) { return; }
+        this._lang.change(code);
+    }
 }
